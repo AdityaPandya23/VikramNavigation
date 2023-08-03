ec8037dd95f65483e98a3c37ba65ba05445ff65c
Aditya Pandya <pandyaaditya600@gmail.com>
2023-08-04 00:36:48 +0530

    implemented forward backward

--- spacecraft.test.js
+++ spacecraft.test.js
@@ -1,5 +1,20 @@
 const spacecraft = require('./spacecraft');
 
+test('should handle missing values correctly', () => {
+  const commands = [];
+  const startingPosition = { y: 0, z: 0 };
+  const initialDirection = 'N';
+
+  const result = spacecraft(
+    commands,
+    startingPosition.x,
+    startingPosition.y,
+    startingPosition.z,
+    initialDirection
+  );
+  expect(result).toEqual('There is error in input');
+});
+
 test('should handle empty commands correctly', () => {
   const commands = [];
   const startingPosition = { x: 0, y: 0, z: 0 };
@@ -28,4 +43,19 @@ test('should handle forward command correctly', () => {
     initialDirection
   );
   expect(result).toEqual({ x: 0, y: 1, z: 0, direction: 'N' });
+});
+
+test('handles backward command correctly', () => {
+  const commands = ['b'];
+  const startingPosition = { x: 0, y: 0, z: 0 };
+  const initialDirection = 'W';
+
+  const result = spacecraft(
+    commands,
+    startingPosition.x,
+    startingPosition.y,
+    startingPosition.z,
+    initialDirection
+  );
+  expect(result).toEqual({ x: 1, y: 0, z: 0, direction: 'W' });
 });
